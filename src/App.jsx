6f19af0d855a48dd213372c24ab1b9aea41adc70
Prajwal Pandey <prajwal.pandey@tensech.com>
2024-01-08 01:17:42 +0530

    refreshing with the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
-import { useState } from 'react'
 import './App.css'
- 
-// let todos = [
-//   {
-//   title: "go to gym",
-//   description: "go to gym at 8:00 pm",
-//   id: "1241351bfjbkj351k32"
-//   },{
-//     title: "go to dinner",
-//     description: "go to dinner at 10:00 pm",
-//     id: "1241351bfjbkj35vsdt232"
-//   },{
-//     title: "wake up",
-//     description: "wake up at 9:00 am in the morning",
-//     id: "1241351bfjbkj351v12342"
-//   }    
-// ];
+
+let todoForToday = {
+  title: "Hit the gym",
+  description: "Go to the gym from 7-9",
+  id: "1"
+}
 
 function App() {
-  const [todo, setTodo] = useState({
-    title: "go to dinner",
-    description: "go to dinner at 10:00 pm",
-    id: "1241351bfjbkj35vsdt232"
-  })
-
-  setInterval(() => {
-    setTodo({
-      title: "Hello, I am changesd",
-      description: "I am descp",
-      id: "Hello, Id would be  - `99913`24"
-    })
-  }, 2000);
   return (
     <div>
       <h1>Hii there </h1>
-      <h5>{todo.title}</h5>
-      <h5>{todo.description}</h5>
-      <h5>{todo.id}</h5>
+      <HelloWorldComponent1></HelloWorldComponent1>
     </div>
   )
 }
+function HelloWorldComponent1(){
+  return <div>
+    {todoForToday.id}
+    {todoForToday.title}
+    {todoForToday.description}
+    <HelloWorldComponent2></HelloWorldComponent2>
+  </div>
+}
 
+function HelloWorldComponent2(){
+  return <div>
+    Hello world 2
+  </div>
+}
 export default App
