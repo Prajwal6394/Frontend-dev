241bafb5950f8bc650e98e9c818f4e401e46f022
Prajwal Pandey <prajwal.pandey@accops.com>
2024-02-06 19:30:07 +0530

    refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,38 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-function useTodo(){
+function useTodo() {
   const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/todos",{
+  function fectTodo() {
+    fetch("http://localhost:3000/todos", {
       method: "GET",
     }).then((response) => {
       response.json().then((data) => {
         console.log(data);
         setTodos(data);
-      })
-    })
-  }, [])
-  return todos; 
+      });
+    });
+  }
+  useEffect(() => {
+    fectTodo();
+  }, []);
+
+  return todos;
 }
 function App() {
-  console.log('above useEffe')
+  console.log("above useEffe");
   const todos = useTodo();
   return (
     <div>
       {todos.map((todo) => {
-        return <div className="container">
-          {todo.title}
-          {todo.description}
-          <button>Delete</button>
-          <br/>
-        </div>
+        return (
+          <div className="container">
+            {todo.title}
+            {todo.description}
+            <button>Delete</button>
+            <br />
+          </div>
+        );
       })}
     </div>
   );
