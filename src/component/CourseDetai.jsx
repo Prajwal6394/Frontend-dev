f9787991769bdaddf9d96fdfde6995a19210a750
Prajwal Pandey <prajwal.pandey@accops.com>
2024-03-19 23:53:00 +0530

    fix recoil issue

--- src/component/CourseDetai.jsx
+++ src/component/CourseDetai.jsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useRecoilState } from 'recoil'; // Import useRecoilState
 import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { CardActionArea } from "@mui/material";
-import {atom, useRecoilValue, useSetRecoilState} from 'recoil';
+import {coursesState} from '../atom/coursesAtom';
 
 
 function CourseDetail() {
-
+  
   let { courseId } = useParams();
-  const setCourses = useSetRecoilState(coursesState)
+  const [courses, setCourses] = useRecoilState(coursesState); // Use Recoil state
+  const [selectedCourse, setSelectedCourse] = useState(null); // State to hold the selected course
   useEffect(() => {
     fetch("http://localhost:3000/admin/courses", {
       headers: {
@@ -29,9 +32,18 @@ function CourseDetail() {
         setCourses(data.courses);
       })
       .catch((error) => console.error("Error fetching courses:", error));
-  }, []);
-  console.log('this is teh api value', coursesState);
- 
+  }, [setCourses]);
+
+  useEffect(() => {
+    if (courses.length > 0) {
+      const course = courses.find(c => c.id === parseInt(courseId));
+      setSelectedCourse(course);
+    }
+  }, [courseId, courses]);
+
+  if (!selectedCourse) {
+    return <div>Loading.....</div>;
+  }
   return (
     <div
       style={{
@@ -42,17 +54,18 @@ function CourseDetail() {
         marginTop: "10rem",
       }}
     >
-      <CourseDetailCard courseId={courseId} />
-      <UpdateCourseCard courseId={courseId} />
+      <CourseDetailCard course={selectedCourse} />
+      <UpdateCourseCard course={selectedCourse} />
     </div>
   );
 }
-function UpdateCourseCard(props) {
 
+function UpdateCourseCard(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [imageLink, setCourseImageLink] = useState("");
-  const [courses, setCourses] = useSetRecoilState(coursesState)
+  const [courses, setCourses] = useRecoilState(coursesState); // Use Recoil state
+
   return (
     <Card
       sx={{ maxWidth: 15000 }}
@@ -116,20 +129,18 @@ function UpdateCourseCard(props) {
             .then((data) => {
               console.log("this is the data", data);
               alert("course Updated");
-              let updatedCourses = [];
-              for(let i = 0; i < courses.length; i++){
-                if(courses[i].id == props.courseId){
-                  updatedCourses.push({
-                    id: props.courseId,
+              const updatedCourses = courses.map(c => {
+                if(c.id === props.course.id) {
+                  return {
+                    ...c,
                     username: title,
                     password: description,
                     imageLink: imageLink
-                  })
-                }else{
-                  updatedCourses.push(props.courses[i]);
+                  };
                 }
-                setCourses(updatedCourses);
-              }
+                return c;
+              });
+              setCourses(updatedCourses);
             });
         }}
       >
@@ -139,42 +150,31 @@ function UpdateCourseCard(props) {
   );
 }
 
-function CourseDetailCard(props) {
-
-  const courses = useRecoilValue(coursesState);
-  let course = null;
-  for (let index = 0; index < courses.length; index++) {
-    if (courses[index].id === parseInt(props.courseId)) {
-      course = courses[index];
-    }
-  }
-  console.log('this the course', courses);
+function CourseDetailCard(prop) {
   return (
     <Card sx={{ maxWidth: 345, marginBottom: "20px" }}>
       <CardActionArea>
         <CardMedia
           sx={{ height: 240 }}
-          image={course.imageLink} // Use the imageLink from the course object
-          title={course.username} // Use the username as the title
+          image={prop.course.imageLink} // Use the imageLink from the course object
+          title={prop.course.username} // Use the username as the title
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {course.username}
+            {prop.course.username}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {course.password}
+            {prop.course.password}
           </Typography>
         </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
       </CardActionArea>
       {/* Use the imageLink from the course object */}
     </Card>
   );
 }
-
-
-const coursesState = atom({
-  key: 'coursesState',
-  default: '', 
-});
 
 export default CourseDetail;
